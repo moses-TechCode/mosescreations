458fefc280256ac1e480a70145a8c02b8a76f66f
Moses techcode <davidkapila30@gmail.com>
2025-04-06 17:23:08 +0200

    Intit

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,13 @@
+function show_navbar(){
+ const show = document.querySelector(".moble_nav")
+ show.style.display="flex"
+}
 
+function hide(){
+  const show = document.querySelector(".moble_nav")
+  show.style.display="none"
+}
+console.log(moses)
+function Contact_page(){
+  
+}
